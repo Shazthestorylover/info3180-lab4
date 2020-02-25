fdf92ddfa405c69426c5fe13b47c5b554df90b65
Ubuntu <ubuntu@ip-172-31-86-147.ec2.internal>
2020-02-25 03:42:30 +0000

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
 # Config Values
-USERNAME = 'admin'
+UPLOAD_FOLDER = './app/static/uploads/'
+USERNAME = 'anonymousUser123'
 PASSWORD = 'password123'
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
-SECRET_KEY = 'Sup3r$3cretkey'
+SECRET_KEY = 'randomk&y1234567890'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
--- app/views.py
+++ app/views.py
@@ -7,7 +7,10 @@ This file creates your application.
 import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
+from .forms import UploadForm
 from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 ###
@@ -32,15 +35,22 @@ def upload():
         abort(401)
 
     # Instantiate your form class
+    imageForm = UploadForm()
 
     # Validate file upload on submit
     if request.method == 'POST':
+        if imageForm.validate_on_submit():
         # Get file data and save to your uploads folder
+            image = imageForm.image.data
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
+            flash('File Saved', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(flash_errors(imageForm))
 
-    return render_template('upload.html')
+    return render_template('upload.html',form = imageForm)
 
 
 @app.route('/login', methods=['POST', 'GET'])
