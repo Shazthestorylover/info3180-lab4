a22602d0e3289fb12af8b59e57c838ca9eb064b3
Shazthestorylover <37606230+Shazthestorylover@users.noreply.github.com>
2020-04-03 13:37:40 -0500

    Revert "simply setting up the necessary tools"
    This reverts commit 56c46a2aa2c0267d8ed7dd2ebfc6b53176966c37.

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 # Config Values
 UPLOAD_FOLDER = './app/static/uploads/'
+USERNAME = 'noOneKnowsMe'
+PASSWORD = 's$cret_Password'
 
-
-# Secret Key
-SECRET_KEY = 'INFO3180$3cretkey'
-
-user = 'Shaz'
-password = 'I_am_an_exceptional_programmer'
-database = 'INFO3180_Project01'
-default = "postgresql://%s:%s@localhost/%s" % (user,password,database) 
-heruko = 'postgresql://emgxrvvcxwkegj:b88d1a34cd8337edfaf89daf766753b402da35f0296f16e66863b063a2f6479b@ec2-3-234-169-147.compute-1.amazonaws.com:5432/dfpsub468628mv'
+# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+SECRET_KEY = 'my_S&cr&t_K&y_u0123456789'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = heruko
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
 app.config.from_object(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,144 +1,143 @@
-""" Additional Info is from info3180 lab 2 and 3. See for more infomation about Flask,
-Jinja2, Werkzeug documentation """
-
-import datetime
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
 import os
-
 from app import app
-from app import db
-from app.models import UserProfile
-from .forms import ProfileForm
-from flask import render_template, request, redirect, url_for, flash
+from .forms import UploadForm
+from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
 
 
-""" Render the home page for the website """
+
+###
+# Routing for your application.
+###
+
 @app.route('/')
 def home():
+    """Render website's home page."""
     return render_template('home.html')
 
 
-
-""" Render the about page for the website """
 @app.route('/about/')
 def about():
-    return render_template('about.html', name = "Chanderpaul Campbell")
-
-
-""" Render the profile page for the website """
-@app.route('/profile' , methods = ['GET','POST'])
-def profile():
-    profileForm = ProfileForm()
-    if request.method == "POST":
-        if profileForm.validate_on_submit():
-            profilepic = profileForm.profilePic.data
-            filename = secure_filename(profilepic.filename)
-            profilepic.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-
-            firstName = profileForm.firstName.data
-            lastName  = profileForm.lastName.data
-            gen = profileForm.gender.data
-            email = profileForm.email.data
-            loc = profileForm.location.data
-            bio = profileForm.biography.data
-            date_joined = format_date_joined()
-
-            user = UserProfile(firstName,lastName,gen,email,loc,bio,filename,date_joined)
-
-            db.session.add(user)
-            db.session.commit()
-
-            
-
-            flash('Profile was created successfully','success')
-            return redirect(url_for('profiles'))
-        else:
-            flash(flash_errors(profileForm))
-            
-    return render_template('profile.html', form = profileForm)
-
-
-""" Render for the profiles page for the website """
-@app.route('/profiles')
-def profiles():
-
-    # users = db.session.query(UserProfile).all() for cloudy 9
-
-    users = UserProfile.query.all()
-
-    return render_template('profiles.html',users = users)
     
+    #"""Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
 
 
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if not session.get('logged_in'):
+        abort(401)
+
+    # Instantiate your form class
+    imageForm = UploadForm()
+
+    # Validate file upload on submit
+    if request.method == 'POST':
+        if imageForm.validate_on_submit():
+            
+            # Get file data and save to your uploads folder
+            image = imageForm.image.data
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
+            flash('File Saved', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(flash_errors(imageForm))
+
+    return render_template('upload.html',form = imageForm)
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid username or password'
+        else:
+            session['logged_in'] = True
+            
+            flash('You were logged in', 'success')
+            return redirect(url_for('upload'))
+    return render_template('login.html', error=error)
 
-""" Render for specific profile page for the website """
 
-@app.route('/profile/<int:userid>') 
-def myprofile(userid = 1):
-
-    users = db.session.query(UserProfile).filter_by(id = userid) #for cloudy 9
-    return render_template('myprofile.html',users = users)
-
-
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out', 'success')
+    
+    return redirect(url_for('home'))
 
 
-
-
-def get_uploaded_images(filename):
+def get_uploaded_images():
     rootdir = os.getcwd()
     filenames = []
     print(rootdir)
     path = '/app/static/uploads'
     for subdir,dirs,files in os.walk(rootdir + path):
+    
         for file in files:
             if file == ".gitkeep":
                 continue;
-            elif file == filename:
-                return filename
+            else:
+                filenames.append(file)
 
+    return filenames
 
-    return "default.png"
- 
-def format_date_joined():
-    date_joined = datetime.datetime.now()
-    return "Joined on " + date_joined.strftime("%B %m, %Y")
+@app.route('/files')
+def files():
 
-""" Flask Functions for application provided by sir """
+    
 
+    files = get_uploaded_images()
+    
+    return render_template('files.html',filenames = files)
+
+        
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+# Flash errors from the form if validation fails
 def flash_errors(form):
-    for field,errors in form.errors.items():
+    for field, errors in form.errors.items():
         for error in errors:
-            flash(u"Error in the %s field - %s" % (getattr(form, field).label.text,error), 'danger')
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+), 'danger')
 
-
-""" Send static text file .. not sure where thou """            
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
+    """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
 
-"""
-    Add headers to both force lastest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-
-"""
 @app.after_request
 def add_header(response):
-    response.headers['X-UA-Compatible'] = 'IE=Edge, chrome = 1'
-    response.headers['Cache-Control'] = 'public, max-age = 0'
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
 
-""" Custom 404 page """
 @app.errorhandler(404)
 def page_not_found(error):
+    """Custom 404 page."""
     return render_template('404.html'), 404
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host = "0.0.0.0", port = "8080")
+    app.run(debug=True, host="0.0.0.0", port="8080")
